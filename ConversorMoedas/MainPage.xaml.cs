acc08dafb0c3d1674a258b6e9b2fb0cbc478db82
lucas.acandido1 <lucas.acandido1@TIT0676447W11-1>
2026-01-27 22:20:39 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorMoedas/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ConversorMoedas
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Dictionary<string, decimal> _toBRL = new()
+        {
+            {"BRL", 1.00m},
+            {"USD", 5.60m },
+            {"EUR", 6.10M }
+
+        };       
+         
+        private readonly Dictionary<string, string> _cultureCurrency = new()
+        {
+            {"BRL", "pt-BR"},
+            {"USD", "en-US" },
+            {"EUR", "de-DE" }
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+    
+        
+        
+    }
+}
